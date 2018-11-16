d1e189c0b87ea1653b0ba0772d46424d4c78d450
chengjy <1977719110@qq.com>
2018-11-16 09:57:24 +0800

    修改pom

--- src/main/java/com/primeton/chengjianyun/demo/DemoApplication.java
+++ src/main/java/com/primeton/chengjianyun/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package com.primeton.chengjianyun.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -10,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
         "com.primeton.chengjianyun.demo.dao"
 })
 @EnableSwagger2
+@EnableTransactionManagement
 public class DemoApplication {
 
     public static void main(String[] args) {
